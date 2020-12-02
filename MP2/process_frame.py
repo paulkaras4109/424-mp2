3a6d89a35ea18c522cf715e019c3c9ee1fe7b1f3
Tyler Mongoven <tpm2@linux-36.ews.illinois.edu>
2020-12-02 17:13:44 -0600

    P2 Finished

--- MP2/process_frame.py
+++ MP2/process_frame.py
@@ -1,21 +1,10 @@
 from scheduling.misc import *
 from scheduling.TaskEntity import *
-import itertools
+
 
 # read the input cluster box data from file
 box_info = read_json_file('../dataset/depth_clustering_detection_flat.json')
 
-
-"""
-Reduce the number of boxes by removing all boxes that are completely contained by another box.
-"""
-
-def remove_nested_boxes(cluster_a, cluster_b):
-    if cluster_b[0] >= cluster_a[0] and cluster_b[1] >= cluster_a[1] and cluster_b[2] <= cluster_a[2] and cluster_b[3] <= cluster_a[3]:
-        return True
-    else:
-        return False
-    
 
 def process_frame(frame):
     """Process frame for scheduling.
@@ -31,17 +20,13 @@ def process_frame(frame):
     Returns:
         A list of task_batches with each task_batch containing some tasks.
     """
-
     
     cluster_boxes_data = get_cluster_box_info(frame, box_info)
 
-    for cluster_a in cluster_boxes_data:
-        for cluster_b in cluster_boxes_data:
-            if remove_nested_boxes(cluster_a, cluster_b):
-                cluster_boxes_data.remove(cluster_b)
-
     task_batches = []
 
+    known_boxes = []
+    
     # student's code here
     for cluster in cluster_boxes_data:
         tmp_coord = []
@@ -73,7 +58,5 @@ def process_frame(frame):
     #print(len(task_batches))
 
     return task_batches
-
-    
 
     
